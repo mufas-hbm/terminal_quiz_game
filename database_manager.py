8c07fcc1f47b890336243a3b835e1badd794297c
Hector Bassas Marin <hector.bassas@dci-student.org>
2025-04-02 09:28:54 +0200

    feat:implement quiz user selection and answer validation - Added menu options for quiz selection. Included topics, modules and submodules for user choice - create_database_dict() in database_manager.py: fetch topics and correspondent modules and submodules and convert into a dict for friendly user selection with numbers instead of strings(category_name). - enabled validation of quiz answers against stored responses.

--- database_manager.py
+++ database_manager.py
@@ -22,5 +22,215 @@ class DatabaseManager:
             print("Error connecting to the database:", e)
 
     def close(self):
-        if self.conn:
-            self.conn.close()
+        try:
+            if self.conn:
+                self.conn.close()
+        except Exception as e:
+            print("Error closing the database:", e)
+    
+    def fetch_submodules_add_questions(self):
+        """
+        Fetches a list of submodule names from the 'submodules' table in the database.
+
+        This method establishes a database connection, executes a query to retrieve
+        all submodule names, and returns the results as a list. In case of an error
+        (e.g., connection failure or query issue), it catches the exception and returns
+        an empty list.
+
+        Returns:
+            list: A list of submodule names fetched from the database. Returns an 
+                empty list if an error occurs during database interaction.
+        """
+        try:
+            with self.conn.cursor() as cursor:  # Automatically closes the cursor after the queries
+                query = """
+                    SELECT submodule_name
+                    FROM submodules;
+                """
+                cursor.execute(query)  # Add a trailing comma to make it a tuple
+                submodules = [row[0] for row in cursor.fetchall()]  # Fetch all rows and extract submodule names
+            return submodules
+        except Exception as e:
+            print("Error fetching submodules:", e)
+            return []  # Return an empty list in case of error
+
+    def fetch_submodules_from_module(self, module=None):
+        """
+        Fetches a list of submodule names from the 'submodules' table in the database.
+
+        This method establishes a database connection, executes a query to retrieve
+        all submodule names, and returns the results as a list. In case of an error
+        (e.g., connection failure or query issue), it catches the exception and returns
+        an empty list.
+
+        Returns:
+            list: A list of submodule names fetched from the database. Returns an 
+                empty list if an error occurs during database interaction.
+        """
+        try:
+            with self.conn.cursor() as cursor:  # Automatically closes the cursor after the queries
+                query = """
+                    SELECT submodule_name
+                    FROM submodules 
+                    JOIN modules 
+                    ON modules.module_id = submodules.module_id 
+                    WHERE module_name = %s;
+                """
+                cursor.execute(query, (module,))  # Add a trailing comma to make it a tuple
+                submodules = [row[0] for row in cursor.fetchall()]  # Fetch all rows and extract submodule names
+            return submodules
+        except Exception as e:
+            print("Error fetching submodules:", e)
+            return []  # Return an empty list in case of error
+
+    
+    def fetch_modules(self, topic):
+        """
+        Fetches a list of module names from the modules' table in the database.
+
+        Args:
+            topic (str): The name of the topic to fetch modules for.
+
+        Returns:
+            list: A list of module names fetched from the database.
+                Returns an empty list if an error occurs during database interaction.
+        """
+        try:
+            with self.conn.cursor() as cursor:
+                query = """
+                    SELECT module_name 
+                    FROM modules 
+                    JOIN topics ON modules.topic_id = topics.topic_id 
+                    WHERE topics.topic_name = %s;
+                """
+                cursor.execute(query, (topic,))  # Pass arguments as a tuple
+                modules = [row[0] for row in cursor.fetchall()]
+            return modules
+        except Exception as e:
+            print(f"Error fetching modules: {e}")
+            return []  # Return an empty list in case of error
+
+    
+    def fetch_topics(self):
+        """
+        Fetches a list of topic names from the topics table in the database.
+
+        This method establishes a database connection, executes a query to retrieve
+        all topic names, and returns the results as a list. In case of an error
+        (e.g., connection failure or query issue), it catches the exception and returns
+        an empty list.
+
+        Returns:
+            list: A list of topic names fetched from the database. Returns an 
+                empty list if an error occurs during database interaction.
+        """
+        try:
+            with self.conn.cursor() as cursor:  
+                query = "SELECT topic_name FROM topics;"
+                cursor.execute(query)
+                topics = [row[0] for row in cursor.fetchall()]  #
+            return topics
+        except Exception as e:
+            print("Error fetching topics:", e)
+            return []  # Return an empty list in case of error
+        
+    def fetch_questions(self, user_choice):
+        try:
+            with self.conn.cursor() as cursor:  # Automatically closes the cursor after the queries
+                if user_choice is None:
+                    query = "SELECT question_text FROM questions WHERE submodule_id IS NULL;"
+                    cursor.execute(query)
+                else:
+                    query = "SELECT question_text FROM questions JOIN submodules ON submodules.submodule_id=questions.submodule_id WHERE submodule_name=%s;"
+                    cursor.execute(query,(user_choice,))
+                questions = [row[0] for row in cursor.fetchall()]  # Fetch all rows and extract submodule names
+            return questions
+        except Exception as e:
+            print("Error fetching questions:", e)
+            return []  # Return an empty list in case of error
+    
+    def fetch_answers_from_question(self, question):
+        try:
+            with self.conn.cursor() as cursor:  # Automatically closes the cursor after the queries
+                query = "SELECT answer FROM answers JOIN questions ON answers.question_id=questions.question_id WHERE question_text=%s;"
+                cursor.execute(query,(question,))
+                answers = [row[0] for row in cursor.fetchall()]  # Fetch all rows and extract submodule names
+            return answers
+        except Exception as e:
+            print("Error fetching questions:", e)
+            return []  # Return an empty list in case of error
+
+    def get_choice(self, category, id, parent_id=None):
+        """
+        Fetches the name for the given category and IDs from the database.
+
+        Args:
+            category (str): The category to fetch (e.g., "topic", "module", "submodule").
+            id (int): The ID of the category.
+            parent_id (int, optional): The parent ID for hierarchical relationships.
+
+        Returns:
+            list: A list of names fetched from the database, or None on failure.
+        """
+        try:
+            with self.conn.cursor() as cursor:
+                if parent_id:
+                    # Define parent column based on category
+                    parent_column = "topic_id" if category == "module" else "module_id" if category == "submodule" else None
+                    if parent_column:
+                        query = f"""
+                            SELECT {category}_name
+                            FROM {category}s
+                            WHERE {category}_id = %s AND {parent_column} = %s;
+                        """
+                        cursor.execute(query, (id, parent_id))
+                else:
+                    query = f"SELECT {category}_name FROM {category}s WHERE {category}_id = %s;"
+                    cursor.execute(query, (id,))
+                
+                output = [row[0] for row in cursor.fetchall()]
+                return output
+        except Exception as e:
+            print(f"Error fetching {category}: {e}")
+            return None
+        
+    def check_user_answer(self, user_answer):
+        try:
+            with self.conn.cursor() as cursor:  # Automatically closes the cursor after the queries
+                query = "SELECT right_answer FROM answers WHERE answer=%s;"
+                print(query)
+                cursor.execute(query,(user_answer, ))
+                answer = cursor.fetchone()[0]
+            return answer
+        except Exception as e:
+            print("Error fetching questions:", e)
+            return []  # Return an empty list in case of error
+
+    def create_database_dict(self):
+        db_dict = {}
+        try:
+            topics = self.fetch_topics()
+            if not topics:
+                return {}  # Return empty dictionary if no topics found
+
+            for topic_index, topic in enumerate(topics, start=1):
+                topic_data = {"name": topic, "modules": {}}
+                modules = self.fetch_modules(topic)
+
+                if modules:
+                    for module_index, module in enumerate(modules, start=1):
+                        module_data = {"name": module, "submodules": []}
+                        submodules = self.fetch_submodules_from_module(module)
+
+                        if submodules:
+                            for submodule_index, submodule in enumerate(submodules, start=1):
+                                module_data["submodules"].append({"name": submodule, "index": submodule_index})
+                        topic_data["modules"][module_index] = module_data
+
+                db_dict[topic_index] = topic_data
+
+            return db_dict
+
+        except Exception as e:
+            print(f"Error creating topics dictionary: {e}")
+            return {}  # Return empty dictionary in case of error
--- quiz.py
+++ quiz.py
@@ -1,51 +1,188 @@
 import random
 from menu import Menu
+from data_input import InputHandler
+from question_manager import QuestionManager
 
 class Quiz:
-    def __init__(self, db_manager):
+    def __init__(self, db_manager,input_handler=None):
         self.db_manager = db_manager
+        self.input_handler = input_handler
+        self.db_dict = self.db_manager.create_database_dict()
 
-    def fetch_questions(self, topic, num_questions=5):
-        try:
-            cursor = self.db_manager.conn.cursor()
-            query = """
-                SELECT question, correct_answer, wrong_answers 
-                FROM quiz_questions WHERE topic = %s 
-                ORDER BY RANDOM() LIMIT %s
-            """
-            cursor.execute(query, (topic, num_questions))
-            return cursor.fetchall()
-        except Exception as e:
-            print("Error fetching questions:", e)
-            return []
+    def start(self,user_choice):
+        #Menu.display_topics_menu(self.db_manager)
+        #print(Menu.create_database_dict(self.db_manager))
+        #QuestionManager.take_category_for_questions(self.db_manager)
+        questions = self.db_manager.fetch_questions(user_choice)
+        for question in questions:
+            print(question)
+            answers = self.db_manager.fetch_answers_from_question(question)
+            for answer in enumerate(answers, start=1):
+                print(f'{answer[0]}: {answer[1]}')
+            answer = int(input("input an answer:"))
+            user_answer = answers[answer-1].strip()
+            check_answer = self.db_manager.check_user_answer(user_answer)
+            print(check_answer)
 
-    def start(self):
-        topic = Menu.display_topic_menu()
-        questions = self.fetch_questions(topic)
 
-        if not questions:
-            print("No questions available for this topic.")
-            return
+    
+    # def prepare_questions(self):
+    #     try:
+    #         play_general_questions = False
 
-        score = 0
-        for idx, (question, correct_answer, wrong_answers) in enumerate(questions, 1):
-            print(f"\nQuestion {idx}: {question}")
+    #         # Fetch topic
+    #         topic_id = int(self.input_handler.get_input("topic"))
+    #         topic_db = self.db_manager.get_choice("topic", topic_id)
+    #         print(topic_db)
+    #         if not topic_db:
+    #             play_general_questions = True
+    #         else:
+    #             # Fetch module filtered by topic_id
+    #             module_id = int(self.input_handler.get_input("module"))
+    #             module_db = self.db_manager.get_choice("module", module_id, parent_id=topic_id)
+    #             print(module_db)
+    #             if not module_db:
+    #                 play_general_questions = True
+    #             else:
+    #                 # Fetch submodule filtered by module_id
+    #                 submodule_id = int(self.input_handler.get_input("submodule"))
+    #                 submodule_db = self.db_manager.get_choice("submodule", submodule_id, parent_id=module_id)
+    #                 print(submodule_db)
+    #                 if submodule_db:
+    #                     return submodule_id
+    #                 else:
+    #                     play_general_questions = True
 
-            # Shuffle and display options
-            options = [correct_answer] + wrong_answers
-            random.shuffle(options)
-            for i, option in enumerate(options, 1):
-                print(f"{i}. {option}")
+    #         if play_general_questions:
+    #             print("You will play with general questions.")
+    #             return None
+    #     except Exception as e:
+    #         print("Error during preparation:", e)
+    #         return None
+                
 
-            # Get user input and check answer
-            try:
-                user_answer = int(input("Your answer (1-4): "))
-                if options[user_answer - 1] == correct_answer:
-                    print("Correct!")
-                    score += 1
-                else:
-                    print(f"Wrong! The correct answer was: {correct_answer}")
-            except ValueError:
-                print("Invalid input! Moving to the next question.")
+    # def prepare_questions(self):
+        # topic = self.input_handler.get_topic().lower().capitalize().strip()
+        # topics_db = self.db_manager.fetch_topics()
+        # play_general_questions = False
+        # prepare_questions = ""
+        # if topic in topics_db:
+        #     module = self.input_handler.get_module().lower().capitalize().strip()
+        #     modules_db = self.db_manager.fetch_modules(topic)
+        #     if module in modules_db:
+        #         #.lower().capitalize().strip() doesn't work for a 2 words string
+        #         submodule = self.input_handler.get_submodule().lower().capitalize().strip()
+        #         submodules_db = self.db_manager.fetch_submodules_from_module(module)
+        #         if submodule in submodules_db:
+        #             prepare_questions = submodule       
+        #         else:
+        #             print("The subtopic you entered doesn't exists, you will play with general questions")
+        #             play_general_questions = True
+        #     else:
+        #         print("The topic you entered doesn't exists, you will play with general questions")
+        #         play_general_questions = True
+        # else:
+        #     print("The topic you entered doesn't exists, you will play with general questions")
+        #     play_general_questions = True
+        #print(prepare_questions)
+        #if play_general_questions:
 
-        print(f"\nYour final score: {score}/{len(questions)}")
+    # def prepare_questions(self):
+    #     try:
+    #         play_general_questions = False
+
+            
+    #         topic = self.input_handler.get_topic().strip().capitalize()
+    #         topics_db = self.db_manager.fetch_topics()
+
+    #         if topic not in topics_db:
+    #             play_general_questions = True
+    #         else:
+    #             module = self.input_handler.get_module()
+    #             modules_db = self.db_manager.fetch_modules(topic)
+
+    #             if module not in modules_db:
+    #                 play_general_questions = True
+    #             else:
+    #                 submodule = self.input_handler.get_submodule().strip()
+    #                 submodules_db = self.db_manager.fetch_submodules_from_module(module)
+
+    #                 if submodule in submodules_db:
+    #                     print(f" You selected {submodule}, let's play")
+    #                     return submodule
+    #                 else:
+    #                     play_general_questions = True
+
+    #         if play_general_questions:
+    #             print("You will play with general questions.")
+    #             return None
+    #     except Exception as e:
+    #         print("Error during preparation:", e)
+    #         return None
+
+    # def take_category_for_questions(self):
+    #     try:
+    #         play_general_questions = False
+
+    #         topic = int(self.input_handler.get_topic())
+
+    #         if topic not in self.db_dict:
+    #             play_general_questions = True
+    #         else:
+    #             module = int(self.input_handler.get_module())
+
+    #             if module not in self.db_dict[topic]['modules']:
+    #                 play_general_questions = True
+    #             else:
+    #                 submodule = int(self.input_handler.get_submodule())
+
+    #                 for sub in self.db_dict[topic]['modules'][module]['submodules']:
+    #                     if sub['index'] == submodule:
+    #                         print(f" You selected {sub['name']}, let's play")
+    #                         return sub['name']  # Return the submodule name
+
+    #                 play_general_questions = True
+
+    #         if play_general_questions:
+    #             print("You will play with general questions.")
+    #             return None
+    #     except Exception as e:
+    #         print("Error during preparation:", e)
+    #         return None
+
+
+
+
+
+
+
+
+
+        # questions = self.fetch_questions(topic)
+
+        # if not questions:
+        #     print("No questions available for this topic.")
+        #     return
+
+        # score = 0
+        # for idx, (question, correct_answer, wrong_answers) in enumerate(questions, 1):
+        #     print(f"\nQuestion {idx}: {question}")
+
+        #     # Shuffle and display options
+        #     options = [correct_answer] + wrong_answers
+        #     random.shuffle(options)
+        #     for i, option in enumerate(options, 1):
+        #         print(f"{i}. {option}")
+
+        #     # Get user input and check answer
+        #     try:
+        #         user_answer = int(input("Your answer (1-4): "))
+        #         if options[user_answer - 1] == correct_answer:
+        #             print("Correct!")
+        #             score += 1
+        #         else:
+        #             print(f"Wrong! The correct answer was: {correct_answer}")
+        #     except ValueError:
+        #         print("Invalid input! Moving to the next question.")
+
+        # print(f"\nYour final score: {score}/{len(questions)}")
